ced00636b8c48cb54cb7ba3995cca816453ccb25
루뎅 <manglilsdsd@gmail.com>
2020-12-20 20:44:55 +0900

    선택지가 인트로앞에 나오지 않도록 함

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
-const tl = gsap.timeline({ defaults: {ease: "power1.out"}   });
 
 
-tl.to('.text', {y:"0%", duration: 1, stagger:0.25 });
+//slider
 
-tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to('.intro', {y: "-100%", duration: 1 }, "-=1");
-tl.fromTo('nav', {opacity:0}, {opacity:1}, {duration: 1});
-tl.fromTo('.big-text', {opacity:0}, {opacity:1}, {duration: 1}, '-=1');
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to('.text', { y: "0%", duration: 1, stagger: 0.25 });
+
+tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to('.intro', { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo('nav', { opacity: 0 }, { opacity: 1 }, { duration: 1 });
+tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1 }, { duration: 1 }, '-=1');
+
+
+
+// WAVE
+class APP {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        document.body.appendChild(this.canvas);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function scrollDisable() {
+    $('html, body').addClass('hidden');
+}
+function scrollAble() {
+    $('html, body').removeClass('hidden');
+}
+
+function scrollDisable() {
+    $('body').addClass('scrollDisable').on('scroll touchmove mousewheel', function (e) {
+        e.preventDefault();
+    });
+}
+function scrollAble() {
+    $('body').removeClass('scrollDisable').off('scroll touchmove mousewheel');
+}
+*/
