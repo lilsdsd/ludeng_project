660a630d3176a44b529d688b5f4df211b576885a
루뎅 <manglilsdsd@gmail.com>
2021-01-03 19:28:41 +0900

    슬라이드 고침

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+//import Vue from "vue";
+//import App from "./App.vue";
 
-Vue.config.productionTip = false;
-
+//Vue.config.productionTip = false;
+/*
 new Vue({
   render: h => h(App)
 }).$mount("#app");
 
-
+*/
 
 function delay(n) {
     n = n || 2000;
@@ -68,4 +68,69 @@ $(function () {
     });
 });
 
+var box = document.querySelector('.start-box'),
+            
+                info = document.querySelector('.info'),
+                // a = box.style.top;
+                y = 600;
+                gravity = 40;
+                up = true;
+                gasokdo = 0;
+                r = 0;
+                o = 100;
+                gasok = true;
+                document.getElementById("mimi_btn1").addEventListener("click", javascript_onclikc);
+                function javascript_onclikc() {
+                    setInterval(function () {
+           
+                        
+                        // if (y > 420) { gravity = -gravity; }
+                       if (up === true) {
+                       
+                             y = y - (gravity + gasokdo);
+                            r = r - 1;
+                            box.style.top = (y) + "px";
+                            box.style.transform = "rotate"+ "("+(r) + "deg"+")";
+                            gasokdo -= 5;
+    
+                            if (gasokdo < -100 ){
+                            o = o - 5;    
+                          
+                            box.style.opacity =  (o)  +"%";  
+                            if (o < 0) {
+                                up = false;
+                              //  element.parentNode.removeChild(box);
+                            }
+                        }
+                        
+                        }
+    
+    
+                        //if (gravity < -10) { up = false;}
+                        /*
+                        if (up == false) {
+                        gravity += 1;
+                        y = y + gravity;
+                        box.style.top = (y) + "px";
+                        }*/
+                    }, 50);
+    
+                }
 
+
+                var swiper = new Swiper('.swiper-container', {
+                    effect: 'coverflow',
+                    grabCursor: true,
+                    centeredSlides: true,
+                    slidesPerView: 'auto',
+                    coverflowEffect: {
+                      rotate: 30,
+                      stretch: 0,
+                      depth: 200,
+                      modifier: 1,
+                      slideShadows: true,
+                    },
+                    pagination: {
+                      el: '.swiper-pagination',
+                    },
+                  });
